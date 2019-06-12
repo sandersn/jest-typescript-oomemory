0f49e6acf811e55f943bfe35f065b5f416b71140
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-06-12 11:08:04 -0700

    Add files needed for oom crash

---
+++ blorgle.test.js
@@ -0,0 +1,17 @@
+// @ts-check
+// var fs = require('fs')
+var ts = require('./typescript.js')
+// test('22=4', () => {
+//     expect(2+2).toBe(4)
+// })
+
+// for (const f of fs.readdirSync('../../ts/tests/cases/conformance/salsa')) {
+//     test(f, () => {
+//         const p = ts.createProgram({ rootNames: ['../../ts/tests/cases/conformance/salsa/' + f], options: { "lib": ["es2015", "dom"] } })
+//         const x = ts.getPreEmitDiagnostics(p)
+//         expect(x.length).toBe(312)
+//         // TODO: Emit baselines
+//         // TODO: Compare them
+//     })
+//     break
+// }
